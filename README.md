aa042c19e54a51c4dc363c6cbf5e646d3145b752
Gautam Mishra <mishra.gautam96@gmail.com>
2018-12-27 16:52:10 +0000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 The Team with No Name
+
+
+## setup routs
+## Add views 
+## create database
+## schema
+## login 
+## register
--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
 var http = require('http');
 var path = require('path');
-
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
 var app = express();
+app.set("view engine", "ejs");
+
+
+app.get('/', function(req, res){
+  res.render("index");
+});
 
 app.get('/', function(req, res){
   res.send("Index page");
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  console.log("App is running ");
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("Server is running ");
 });
