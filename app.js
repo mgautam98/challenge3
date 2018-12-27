51c5070a0c5dd00f0830c88940c6fd99ae154076
Gautam Mishra <mishra.gautam96@gmail.com>
2018-12-27 20:18:21 +0000

    add login and register routes

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ app.get('/', function(req, res){
   res.render("index");
 });
 
+app.get('/register', function(req, res){
+  res.render("register");
+});
+
+app.get('/login', function(req, res){
+  res.render("login");
+});
+
+
+
+
+
 app.get("*", function(req, res){
   res.send("Oops! Something went wrong.")
 })
