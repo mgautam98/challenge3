2db6426a0d3542dd33feb17d4f0eb0ddb0fa0b33
Gautam Mishra <mishragautam96@gmail.com>
2019-02-10 08:18:05 +0000

    refactor routes start

---
+++ routes/likes.js
@@ -0,0 +1,16 @@
+var express                     = require('express'),
+    router                      = express.Router(),
+    middleware                  = require("./middleware");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+module.exports = router;
